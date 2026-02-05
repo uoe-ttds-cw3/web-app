062c29da978f0529d33da9c50adfd52635403352
jasiene <jasien_elimy@hotmail.co.uk>
2026-02-05 11:39:59 +0000

    Update index.tsx

--- src/features/search/components/SearchForm/index.tsx
+++ src/features/search/components/SearchForm/index.tsx
@@ -1,7 +1,17 @@
-import { Input, InputGroup } from "@chakra-ui/react";
+import { Box, Button, Input, InputGroup } from "@chakra-ui/react";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 export const SearchForm = () => {
+  const categories = ["A", "B"]; // probs come from a fetch
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  // using react hook form
+  // in onValid callback func
+  // construct request to include search term and selected category
+
   return (
     <InputGroup
       startElement={<FaSearch color="#4CAF50" style={{ marginLeft: "16px" }} />}
@@ -10,6 +20,12 @@ export const SearchForm = () => {
         colorPalette="green"
         placeholder="Search by manufacturer, material, recall event"
       ></Input>
+      <Box>
+        {categories.map((category) => (
+          // Custom component
+          <Box></Box>
+        ))}
+      </Box>
     </InputGroup>
   );
 };
